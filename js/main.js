b720fec3ea7f721ec2678f439a701923f99d0f43
cnakpil <85041294+cnakpil@users.noreply.github.com>
2022-02-01 10:17:19 -0700

    Total restructuring of code, adding portfolio to landing.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Import Highway
+// import Highway from '@dogstudio/highway';
+
+// Call Highway.Core once.
+// Store it in a variable to use events
+// const H = new Highway.Core({[...]});
+
+// make span clickable INCOMPLETE
+// let span = document.getElementsByClassName("navLink");
+// for (i=0; i<3; i++){
+//   span[i].addEventListener("click", function(){
+//     window.open()
+//   });
+// }
